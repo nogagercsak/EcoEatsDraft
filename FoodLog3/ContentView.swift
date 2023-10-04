86db4555fdca668e0b788bbceb30b6d2f248cb0b
Noga <nogah0317@gmail.com>
2023-10-03 20:56:12 -0400

    fixed the at back to normal

--- FoodLog3/ContentView.swift
+++ FoodLog3/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item a \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
